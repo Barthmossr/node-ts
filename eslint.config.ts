9b3dbafa6591c6a4ac5a25386533c298fa2539f1
Barthmossr <barthmossr@gmail.com>
2025-11-29 15:35:30 -0300

    chore: add prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,33 @@
-import js from "@eslint/js"
-import globals from "globals"
-import tseslint from "typescript-eslint"
-import { defineConfig } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    ignores: ['dist/**'],
   },
-  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js, prettier: eslintPluginPrettier },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': 'error',
+      semi: ['error', 'never'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    extends: [tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
 ])
